57db29433e6b0cad6136f8bb6606eb5a37cc4fe2
oviya <oviya@csil.cs.ucsb.edu>
2022-11-30 11:40:15 -0800

    first submission of practice w/ all functions drafted

--- code.cpp
+++ code.cpp
@@ -1,5 +1,5 @@
-// Write your name
-// Write your perm number
+// Write your name: OVIYA SEENIRAJ
+// Write your perm number: 3400652
 // Do not include a main function in this file
 // Only submit this file
 // See code.h for the descriptions of each problem
@@ -7,9 +7,28 @@
 
 
 /* Problem 1: 10 pts */
- 
+
+int max = 0;
+Node* biggestValue = nullptr;
+int numIterated = 0;
 TeaPacket* bestPacket(TeaPacket* head){
-   return 0;
+   if (head==nullptr && numIterated = 0) //if list empty
+   {
+      return 0;
+   }
+   else if (head == nullptr) //if reached end of list
+   {
+      return biggestValue;
+   }
+
+   int value = head->rating * head->rarity;
+   if (value > max) // if we find a biggestValue node
+   {
+      biggestValue = head;
+      max = value;
+   }
+   numIterated++;
+   bestPacket(head->next);
 }
 
 
@@ -17,13 +36,55 @@ TeaPacket* bestPacket(TeaPacket* head){
 /* Problem 2: 10 points*/
 
 Node* insert(Node* head, int value){
-   return 0;
+   Node* currNode = head;
+   while (currNode != nullptr) // while we still have values
+   {
+      Node* toInsert = nullptr; // creating node of given data
+      toInsert->data = value;
+      if (currNode->next == nullptr) // if to be placed at end of list
+      {
+         currNode->next == toInsert;
+      }
+      else if (currNode->data > value) // if to be placed before head
+      {
+         toInsert->next = head;
+         return toInsert;
+      }
+      else if (currNode->next->data > value) // 'normal' case, if to be placed at the next spot
+      {
+         Node* temp = currNode->next;
+         currNode->next = toInsert;
+         toInsert->next = temp;
+         return head;
+      }
+      else // if place not found yet
+      {
+         currNode = currNode->next;
+      }
+   }
+   return head;
 }
 
 /* Problem 3: 10 points*/
 
 bool isBalanced(std::string s){
-   
+   int countA = 0;
+   int countB = 0;
+   for (int i = 0; i < s.length(); i++)
+   {
+      if (string[i] == 'A')
+      {
+         countA++;
+      }
+      else if (string[i] == 'B')
+      {
+         countB++;
+      }
+   }
+   if (countA == countB)
+   {
+      return true;
+   }
    return false;
 }
 
